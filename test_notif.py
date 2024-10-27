7efab551b9df49a75e52fc53168827e01494c9a7
Ho3einMokhtari <hosseinmokhtari5555@gmail.com>
2024-10-27 20:08:59 +0330

    Test Notif

--- test_notif.py
+++ test_notif.py
@@ -4,21 +4,29 @@ import requests
 import threading
 import config
 from melipayamak import Api
+from telethon.sync import TelegramClient, Button
 
 # تنظیمات تلگرام
 TELEGRAM_API_URL = f"https://api.telegram.org/bot{config.BOT_TOKEN}/sendMessage"
 
 # لیست ادمین‌ها
 ADMINS_LIST = config.ADMINS_LIST
-
-def send_telegram_message(chat_id, text):
-    payload = {
-        'chat_id': chat_id,
-        'text': text,
-        'parse_mode': 'HTML'
-    }
-    response = requests.post(TELEGRAM_API_URL, data=payload)
-    print(response)
+def send_telegram_message(chat_id, text, ex, username):
+    # payload = {
+    #     'chat_id': chat_id,
+    #     'text': text,
+    #     'parse_mode': 'HTML'
+    # }
+    # requests.post(TELEGRAM_API_URL, data=payload)
+    keys = None
+    if ex:
+        keys = [
+            Button.inline("🔋تمدید اشتراک", data=str.encode("sr_inf:" + str(username)))
+        ]
+    cli = TelegramClient("cli", config.API_ID, config.API_HASH)
+    cli.start(bot_token=config.BOT_TOKEN)
+    cli.send_message(chat_id, text, buttons=keys)
+    cli.disconnect()
 def check_services():
     conn = sqlite3.connect('bot.db')  # نام دیتابیس خود را اینجا قرار دهید
     cursor = conn.cursor()
@@ -39,15 +47,20 @@ def check_services():
                     continue
             if current_time > end:
                 # ارسال پیام به کاربر
-                user_message = f"""یادآوری تمدید اشتراک کاربر گرامی؛ حساب کاربری {username} شما در حال اتمام اعتبار است و بیش از 90% از اعتبار آن مصرف شده است. هم اکنون برای تمدید اقدام کنید.
- kooll.online"""
-                send_telegram_message(user_id, user_message)
+                user_message = f"""❗️ پایان اشتراک تست{username}
+📍 حجم یا تاریخ اعتبار این اشتراک به پایان رسیده است و این اشتراک به صورت خودکار غیرفعال گردیده است!
+
+♻️ جهت فعالسازی مجدد اشتراک خود از دکمه زیر  اقدام به مشاهده اشتراک خود کنید.
+
+❌ در صورت عدم تمدید اشتراک 3 روز پس از پایان مدت اعتبار اشتراک به صورت خودکار توسط ربات حذف خواهد شد
+"""
+                send_telegram_message(user_id, user_message, True, username=username)
                 cursor.execute(f"UPDATE test_account SET send_notification = {True} WHERE username = '{username}'")
                 conn.commit()
                 # ارسال پیام به ادمین‌ها
                 admin_message = f"ادمین گرامی، بیش از 90 درصد از اعتبار سرویس با نام کاربری {username} مصرف شده است\nآیدی عددی کاربر: {user_id}"
                 for admin_id in ADMINS_LIST:
-                    send_telegram_message(admin_id, admin_message)
+                    send_telegram_message(admin_id, admin_message, ex=False, username=username)
 
         time.sleep(60)  # هر 1 دقیقه چک کند
 
